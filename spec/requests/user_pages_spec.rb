adff227d0109249cb0e2429461ce79ba4107fa65
laudbrian <LAU@LAUs-MacBook-Air.local>
2014-03-03 02:40:43 -0800

    Finish user edit, update, index, and destroy actions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,56 +4,37 @@ describe "User pages" do
 
   subject { page }
 
-  describe "signup page" do
-  	before { visit signup_path }
+  describe "index" do
 
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
-  end
+    let(:user) { FactoryGirl.create(:user) }
 
-  describe "profile page" do
-  	let(:user) { FactoryGirl.create(:user) }
-  	before { visit user_path(user) }
-
-    it { should have_content(user.name) }
-    it { should have_title(user.name) }
-  end
-
-  describe "signup" do
-    
-    before { visit signup_path }
-
-    let(:submit) { "Create my account" }
-
-    describe "with invalid information" do
-      it "should not create a user" do
-        expect { click_button submit }.not_to change(User, :count)
-      end
+    before do
+      sign_in user
+      visit users_path
     end
 
-    describe "with valid information" do
+    it { should have_title('All users') }
+    it { should have_content('All users') }
 
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+    describe "delete links" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect do
+            click_link('delete', match: :first)
+          end.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
       end
-    
-
-      it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
-      end
-
-      describe "after saving the user" do
-        before { click_button submit }
-        
-        let(:user) { User.find_by(email: 'user@example.com') }
-
-        it { should have_link('Sign out') }
-        it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-      end 
     end
-  end 
+  end
 end
