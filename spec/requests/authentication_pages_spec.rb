99815a79949a78e3be540b422405799900abc163
laudbrian <laudbrian@gmail.com>
2014-06-10 16:38:02 -0700

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -27,22 +27,43 @@ describe "Authentication" do
       end
     end
 
-    describe "with valid information" do
-      let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
+    describe "authorization" do
+
+      describe "for non-signed-in users" do
+        let(:user) { FactoryGirl.create(:user) }
+
+        describe "in the Microposts controller" do
+
+          describe "submitting to the create action" do
+            before { post microposts_path }
+            specify { expect(response).to redirect_to(signin_path) }
+          end
+
+          describe "submitting to the destroy action" do
+            before { delete micropost_path(FactoryGirl.create(:micropost)) }
+            specify { expect(response).to redirect_to(signin_path) }
+          end
+        end
       end
 
-      it { should have_title(user.name) }
-      it { should have_link('Profile',     href: user_path(user)) }
-      it { should have_link('Sign out',    href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
-      
-      describe "followed by signout" do
-        before { click_link "Sign out" }
-        it { should have_link('Sign in') }
+      describe "with valid information" do
+        let(:user) { FactoryGirl.create(:user) }
+        before do
+          fill_in "Email",    with: user.email.upcase
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+
+        it { should have_title(user.name) }
+        it { should have_link('Profile',     href: user_path(user)) }
+        it { should have_link('Settings',    href: edit_user_path(user)) }
+        it { should have_link('Sign out',    href: signout_path) }
+        it { should_not have_link('Sign in', href: signin_path) }
+        
+        describe "followed by signout" do
+          before { click_link "Sign out" }
+          it { should have_link('Sign in') }
+        end
       end
     end
   end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,38 +3,55 @@ require 'spec_helper'
 describe "User pages" do
 
   subject { page }
-
-  describe "index" do
-
+  
+  describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
 
-    before do
-      sign_in user
-      visit users_path
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
     end
 
-    it { should have_title('All users') }
-    it { should have_content('All users') }
+    describe "index" do
 
-    describe "delete links" do
+      let(:user) { FactoryGirl.create(:user) }
 
-      it { should_not have_link('delete') }
+      before do
+        sign_in user
+        visit users_path
+      end
 
-      describe "as an admin user" do
-        let(:admin) { FactoryGirl.create(:admin) }
-        before do
-          sign_in admin
-          visit users_path
+      it { should have_title('All users') }
+      it { should have_content('All users') }
+
+      describe "delete links" do
+
+        it { should_not have_link('delete') }
+
+        describe "as an admin user" do
+          let(:admin) { FactoryGirl.create(:admin) }
+          before do
+            sign_in admin
+            visit users_path
+          end
+
+          it { should have_link('delete', href: user_path(User.first)) }
+          it "should be able to delete another user" do
+            expect do
+              click_link('delete', match: :first)
+            end.to change(User, :count).by(-1)
+          end
+          it { should_not have_link('delete', href: user_path(admin)) }
         end
-
-        it { should have_link('delete', href: user_path(User.first)) }
-        it "should be able to delete another user" do
-          expect do
-            click_link('delete', match: :first)
-          end.to change(User, :count).by(-1)
-        end
-        it { should_not have_link('delete', href: user_path(admin)) }
       end
     end
-  end
+  end  
 end
