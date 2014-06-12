229c548011a33ead53d3aef286afaf519f9faf36
laudbrian <laudbrian@gmail.com>
2014-06-11 21:07:33 -0700

    Add user following

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -20,38 +20,82 @@ describe "User pages" do
       it { should have_content(user.microposts.count) }
     end
 
-    describe "index" do
+    describe "follow/unfollow buttons" do
+      let(:other_user) { FactoryGirl.create(:user) }
+      before { sign_in user }
 
-      let(:user) { FactoryGirl.create(:user) }
+      describe "following a user" do
+        before { visit user_path(other_user) }
 
-      before do
-        sign_in user
-        visit users_path
+        it "should increment the followed user count" do
+          expect do
+            click_button "Follow"
+          end.to change(user.followed_users, :count).by(1)
+        end
+
+        it "should increment the other user's followers count" do
+          expect do
+            click_button "Follow"
+          end.to change(other_user.followers, :count).by(1)
+        end
+
+        describe "toggling the button" do
+          before { click_button "Follow" }
+          it { should have_xpath("//input[@value='Unfollow']") }
+        end
       end
 
-      it { should have_title('All users') }
-      it { should have_content('All users') }
+      describe "unfollowing a user" do
+        before do
+          user.follow!(other_user)
+          visit user_path(other_user)
+        end
 
-      describe "delete links" do
+        it "should decrement the followed user count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(user.followed_users, :count).by(-1)
+        end
 
-        it { should_not have_link('delete') }
+        it "should decrement the other user's followers count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(other_user.followers, :count).by(-1)
+        end
 
-        describe "as an admin user" do
-          let(:admin) { FactoryGirl.create(:admin) }
-          before do
-            sign_in admin
-            visit users_path
-          end
-
-          it { should have_link('delete', href: user_path(User.first)) }
-          it "should be able to delete another user" do
-            expect do
-              click_link('delete', match: :first)
-            end.to change(User, :count).by(-1)
-          end
-          it { should_not have_link('delete', href: user_path(admin)) }
+        describe "toggling the button" do
+          before { click_button "Unfollow" }
+          it { should have_xpath("//input[@value='Follow']") }
         end
       end
     end
-  end  
+  end
+
+  describe "following/followers" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:other_user) { FactoryGirl.create(:user) }
+    before { user.follow!(other_user) }
+
+    describe "folllowed users" do
+      before do
+        sign_in user
+        visit following_user_path(user)
+      end
+
+      it { should have_title(full_title('Following')) }
+      it { should have_selector('h3', text: 'Following') }
+      it { should have_link(other_user.name, href: user_path(other_user)) }
+    end
+
+    describe "followers" do
+      before do
+        sign_in other_user
+        visit followers_user_path(other_user)
+      end
+
+      it { should have_title(full_title('Followers')) }
+      it { should have_selector('h3', text: 'Followers') }
+      it { should have_link(user.name, href: user_path(user)) }
+    end
+  end
 end
